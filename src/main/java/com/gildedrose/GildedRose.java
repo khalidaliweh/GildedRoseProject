341e98e4670991abfdb0c1f7d197a2437d65235c
khalidaliweh <khalidaliweh@hotmail.com>
2016-09-28 17:41:10 -0400

    commit test

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,10 +14,12 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals(SULFURAS))
-                continue;
 
+        for (int i = 0; i < items.length; i++) {
+                //SULFURAS
+            if (items[i].name.equals(SULFURAS)){}
+
+                //AGED_BRIE
             else if (items[i].name.equals(AGED_BRIE)) {
                 items[i].sellIn = items[i].sellIn - 1;
 
@@ -30,53 +32,46 @@ class GildedRose {
                 }
 
                 continue;
-            } else if (items[i].name.equals(BACKSTAGE_PASSES)) {
+
+            }
+            // BACKSTAGE_PASSES
+            else if (items[i].name.equals(BACKSTAGE_PASSES)) {
 
                 if (items[i].quality < MAX_QUALITY) {
 
                     changeQuality(items[i], 1);
 
 
-                        if (items[i].sellIn < ELEVEN_DAYS) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                changeQuality(items[i], 1);
-                            }
-                        }
-
-                        if (items[i].sellIn < SIX_DAYS) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                changeQuality(items[i], 1);
-                            }
-                        }
-
-                        if (items[i].sellIn <= 0) {
-                            items[i].quality = 0;
+                    if (items[i].sellIn < ELEVEN_DAYS) {
+                        if (items[i].quality < MAX_QUALITY) {
+                            changeQuality(items[i], 1);
                         }
                     }
-               continue;
+
+                    if (items[i].sellIn < SIX_DAYS) {
+                        if (items[i].quality < MAX_QUALITY) {
+                            changeQuality(items[i], 1);
+                        }
+                    }
+
+                    if (items[i].sellIn <= 0) {
+                        items[i].quality = 0;
+                    }
+                }
+                continue;
             }
+            //Anything else
             else {
                 items[i].sellIn = items[i].sellIn - 1;
                 if (items[i].quality > 0) {
-                    if(items[i].sellIn < 0) {
+                    if (items[i].sellIn < 0) {
                         changeQuality(items[i], -2);
-                    }
-                    else {
+                    } else {
                         changeQuality(items[i], -1);
                     }
                 }
 
             }
-
-
-
-
-            // all items other than SULFRAS decrease
-
-
-
-
-
         }
 
     }
